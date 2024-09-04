78f60192042d4563ceaea01ae1095ef0f3a6dc06
taeyomi <100909703+taeyomi@users.noreply.github.com>
2024-09-04 22:32:40 +0900

    🚀 feat: GraphQL 서버와 통신하는 프론트엔드 설정

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -3,20 +3,25 @@ import './App.css';
 import './movie.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './movie.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import MovieSearch from './movie.js';
 import MovieDetail from './movieDetail.js';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <Router>
     <div className="App">
    
       <Routes>
       <Route path="/" element={<MovieSearch />} />
-          <Route path="/movie/:id" element={<MovieDetail />} />
+         
         </Routes>
         </div>
     </Router>
+    </QueryClientProvider>
   );
 }
 
